93e9a06adff5df19993528791cfd6cd2e77aae86
charles <c.wolfe132@gmail.com>
2019-10-22 19:01:00 -0700

    Made python node work for any range

--- laser_scan.py
+++ laser_scan.py
@@ -4,13 +4,13 @@ import rospy
 from sensor_msgs.msg import LaserScan
 
 def callback(msg):
-	print 'Lidar reading directly to the right: '
+	print 'Left distance reading: '
 	print msg.ranges[0]
-	print 'Lidar reading directly ahead: '
-	print msg.ranges[44]
-	print 'Lidar reading directly to the left: '
-	print msg.ranges[89]
+	print 'Straight ahead distance reading: '
+	print msg.ranges[len(msg.ranges)/2]
+	print 'Right distance reading: '
+	print msg.ranges[len(msg.ranges)-1]
 
 rospy.init_node('scan_values')
-sub = rospy.Subscriber('/base_scan', LaserScan, callback)
+sub = rospy.Subscriber('/scan', LaserScan, callback)
 rospy.spin()
