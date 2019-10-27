5496c4dd3a0a2b7259805b3fdd5224a02543df83
charles <c.wolfe132@gmail.com>
2019-10-26 18:16:35 -0700

    Added range function

--- laser_scan.py
+++ laser_scan.py
@@ -3,6 +3,11 @@
 import rospy 
 from sensor_msgs.msg import LaserScan
 
+def in_range(range):
+	for i in msg.ranges:
+		if msg.ranges[i] < range:
+			print 'Object in range'
+
 def callback(msg):
 	print 'Left distance reading: '
 	print msg.ranges[0]
