a64abc87a07b9cf0dfa1bedda5144a41855ee1bd
charles <c.wolfe132@gmail.com>
2019-11-26 18:36:06 -0800

    Code works now

--- lidar/src/lidar_scan.cpp
+++ lidar/src/lidar_scan.cpp
@@ -1,16 +1,17 @@
 #include <ros/ros.h>
 #include "sensor_msgs/LaserScan.h"
-//#include "sensor_msgs/   "
+#include "sensor_msgs/PointCloud2.h"
 
-float distance_at_index(sensor_msgs::LaserScan &msg, int x){
-	return msg.ranges[x];
+//float distance_at_index(sensor_msgs::PointCloud2 &msg, int x){
+	//return msg.ranges[x];
 	//return msg.data[x];
-}
+//}
 
-void callback_scan(sensor_msgs::LaserScan msg){
-	std::cout << "Distance at minimum angle: " << distance_at_index(msg, 0) << "\n";
-	std::cout << "Distance at middle angle: " << distance_at_index(msg, msg.ranges.size()/2) << "\n";
-	std::cout << "Distance at maximum angle: " << distance_at_index(msg, msg.ranges.size()-1) << "\n";
+void callback_scan(sensor_msgs::PointCloud2 msg){
+	std::cout << msg.data[0] << std::endl;
+	//std::cout << "Distance at minimum angle: " << distance_at_index(msg, 0) << "\n";
+	//std::cout << "Distance at middle angle: " << distance_at_index(msg, msg.ranges.size()/2) << "\n";
+	//std::cout << "Distance at maximum angle: " << distance_at_index(msg, msg.ranges.size()-1) << "\n";
 }
 
 int main (int argc, char **argv)
@@ -18,7 +19,7 @@ int main (int argc, char **argv)
 	ros::init(argc, argv, "lidar_scan");		
 	ros::NodeHandle nh;
 
-	ros::Subscriber sub = nh.subscribe("/scan", 10, callback_scan);		// change topic
+	ros::Subscriber sub = nh.subscribe("/LeddarTech_1/scan_cloud", 10, callback_scan);		// change topic
 
 	ros::spin();		
 }
