9e691220b5d9ffadf5a9223469de3dca6b1a0148
Brandon Rice <bjrice13@gmail.com>
2020-01-10 19:23:10 -0700

    Added multi-input support with color-coded terminal output

--- learning_joy/src/joystick_handler.cpp
+++ learning_joy/src/joystick_handler.cpp
@@ -12,16 +12,52 @@
 // and
 // rosrun learning_joy turtle_teleop_joy
 
+enum joystickInputs
+{
+  // Digital
+  A,
+  B,
+  X,
+  Y,
+  L1,
+  R1,
+  L2,
+  R2,
+  Share,
+  Options,
+  Home,
+  L3,
+  R3,
+  // Analog
+  LSX,
+  LSY,
+  L2Analog,
+  RSX,
+  RSY,
+  R2Analog,
+  HatX,
+  HatY
+};
+
+// struct joystick_values
+// {
+//   float value[21];
+// };
+
 
 class JoystickHandler
 {
 public:
-  JoystickHandler();
+  JoystickHandler(int);
+  float getValue(joystickInputs);
+  int id;
 
 private:
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
   ros::NodeHandle nh_;
+
+  float values[21];
 
   int linear_, angular_;
   double l_scale_, a_scale_;
@@ -31,7 +67,7 @@ private:
 };
 
 // Set-up subscribers/publishers in constructor
-JoystickHandler::JoystickHandler():
+JoystickHandler::JoystickHandler(int joynum):
   linear_(1),
   angular_(2)
 {
@@ -41,14 +77,42 @@ JoystickHandler::JoystickHandler():
   nh_.param("scale_linear", l_scale_, l_scale_);
 
   // Get joystick info from /joy
-  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &JoystickHandler::joyCallback, this);
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("j"+std::to_string(joynum), 10, &JoystickHandler::joyCallback, this);
 
   // Set joystick values in /cmd_vel
   vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+
+  id = joynum;
+}
+
+float JoystickHandler::getValue(joystickInputs i)
+{
+  return values[i];
 }
 
 void JoystickHandler::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
+  if(id == 0)
+  {
+    std::cout << "\033[1;33m";
+  }
+  else if(1)
+  {
+    std::cout << "\033[1;36m";
+  }
+  for(int i = 0; i < 8; i++)
+  {
+    values[i] = joy->axes[i];
+    printf("%f ", values[i]);
+  }
+  for(int i = 0; i < 13; i++)
+  {
+    values[i+8] = joy->buttons[i];
+    printf("%i ", (int)values[i+8]);
+  }
+  std::cout << "\033[0m\n";
+  // printf("\n");
+
   geometry_msgs::Twist twist;
   twist.angular.z = joy->axes[angular_]; // Set rotation axis data
   twist.linear.x = joy->axes[linear_]; // Set forward/backward axis data
@@ -63,7 +127,7 @@ void callback(const geometry_msgs::Twist::ConstPtr& msg){
     leftVelocity = linear - TRACK_WIDTH * angular/2;
     rightVelocity = linear + TRACK_WIDTH * angular/2;
 
-    ROS_INFO("%f, %f", leftVelocity, rightVelocity);
+    // ROS_INFO("%f, %f", leftVelocity, rightVelocity);
 }
 
 
@@ -71,7 +135,8 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "joy_handler");
 
-  JoystickHandler joy_handler;
+  JoystickHandler joy_handler(0);
+  JoystickHandler joy_handler1(1);
 
   // Handler for setting calcualted velocity values
   ros::NodeHandle vel_handle;
