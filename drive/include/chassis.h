5bf8c83cdcc2f2bc01dca92a5ddd3f6b7889abf0
Brandon Rice <bjrice13@gmail.com>
2020-02-21 00:20:15 -0700

    Added README documentation and comments for drive package

--- drive/include/chassis.h
+++ drive/include/chassis.h
@@ -18,82 +18,65 @@
 
 class Chassis
 {
-  // private:
-  //   // ODOM, Current Orientation Data
-  //   static std::string cont_port_1;
-  //   static std::string cont_port_2;
-  //
-  //   static ros::NodeHandle nh_l;
-  //   static ros::NodeHandle nh_r;
-  //
-  //   static ros::Publisher drive_left_control;
-  //   static ros::Publisher drive_right_control;
-  //
-  //   static std::string left_command;
-  //   static std::string right_command;
-  //
-  //   static std_msgs::String str_msg_l;
-  //   static std_msgs::String str_msg_r;
-  //
-  // public:
-  //   Chassis(int = 0, int = 1);
-  //
-  //   static void update()
-  //   {
-  //     ros::Publisher pub_l = nh_l.advertise<std_msgs::String>("drive_left_control", 1000);
-  //     ros::Publisher pub_r = nh_r.advertise<std_msgs::String>("drive_right_control", 1000);
-  //
-  //     while (ros::ok())
-  //     {
-  //       pub_l.publish(str_msg_l);
-  //       pub_r.publish(str_msg_r);
-  //
-  //       ros::spinOnce();
-  //     }
-  //   }
+
 public:
 
+  // floats to store most-recent data from joystick subscriber
   inline static float joyDataLeft;
   inline static float joyDataRight;
 
+  // Joystick data subscriber callback to assign member variables with data
   static void chatterCallback(const sensor_msgs::Joy::ConstPtr& joy)
   {
-    joyDataLeft = joy->axes[1];
-    joyDataRight = joy->axes[4];
+    joyDataLeft = joy->axes[1];  // Left Y Axis
+    joyDataRight = joy->axes[4]; // Right Y Axis
   }
 
   static void update()
   {
+    // Subscriber node to handle joystick input data
     ros::NodeHandle joy_handle;
     ros::Subscriber sub = joy_handle.subscribe("/j0", 1000, chatterCallback);
 
-    serial::Serial my_serial_l("/dev/ttyACM0", 115200, serial::Timeout::simpleTimeout(10));
-    serial::Serial my_serial_r("/dev/ttyACM1", 115200, serial::Timeout::simpleTimeout(10));
+    // Serial objects used to communicate with RoboteQ motor controllers
+    serial::Serial my_serial_l("/dev/ttyACM0", 115200, serial::Timeout::simpleTimeout(10)); // 10 ms timeout
+    serial::Serial my_serial_r("/dev/ttyACM1", 115200, serial::Timeout::simpleTimeout(10)); // 10 ms timeout
 
+    // Initial debug statements to check for left motor controller port opened
     std::cout << "Is the serial port open?";
     if(my_serial_l.isOpen())
       std::cout << " Yes." << std::endl;
     else
       std::cout << " No." << std::endl;
 
+    // String used to fill with data and send over serial
     std::string data;
     ros::Rate loop_rate(1000);
 
     while(ros::ok())
     {
-      std::string result = my_serial_l.read(500);
-       result += my_serial_r.read(500);
+      std::string result = my_serial_l.read(500); // Read "all" of left motor controller feedback buffer
+      result += my_serial_r.read(500);  // Read "all" of right motor controller feedback buffer
 
-std::cout << "Bytes read: ";
-std::cout << result.length() << ", String read: " << result << std::endl;
+      // Display feedback info from both motor controllers
+      std::cout << "Bytes read: ";
+      std::cout << result.length() << ", String read: " << result << std::endl;
 
-data = "!G 1 " + std::to_string(joyDataLeft * 1000) + "\r\n!G 2 " + std::to_string(joyDataLeft * 1000) + "\r\n";// + std::to_string(joyDataLeft) + "\r\n";
-  size_t bytes_wrote = my_serial_l.write(data);
-data = "!G 1 " + std::to_string(joyDataRight * 1000) + "\r\n!G 2 " + std::to_string(joyDataRight * 1000) + "\r\n";// + std::to_string(joyDataLeft) + "\r\n";
-  bytes_wrote += my_serial_r.write(data);
-        std::cout << "BYTES: " << bytes_wrote << " ; command: " << data << std::endl;
-        ros::spinOnce();
-        loop_rate.sleep();
+      // Send new joystick data to motor controllers
+      // Write to left motor controller
+      data = "!G 1 " + std::to_string(joyDataLeft * 1000) + "\r\n!G 2 " + std::to_string(joyDataLeft * 1000) + "\r\n";// + std::to_string(joyDataLeft) + "\r\n";
+      size_t bytes_wrote = my_serial_l.write(data);
+
+      // Write to right motor controller
+      data = "!G 1 " + std::to_string(joyDataRight * 1000) + "\r\n!G 2 " + std::to_string(joyDataRight * 1000) + "\r\n";// + std::to_string(joyDataLeft) + "\r\n";
+      bytes_wrote += my_serial_r.write(data);
+
+      // Verify the data was sent correctly
+      std::cout << "BYTES: " << bytes_wrote << " ; command: " << data << std::endl;
+
+      // Delay for threaded looping
+      ros::spinOnce();
+      loop_rate.sleep();
     }
   }
 };
