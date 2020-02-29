ec2aecea0d850ce26451a30b1a0e00a798af2900
Brandon Rice <bjrice13@gmail.com>
2020-02-28 17:57:26 -0700

    Deleted /buildold

--- buildold/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brandon/catkin_ws/src/urc-code/src
-# Build directory: /home/brandon/catkin_ws/src/urc-code/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("image_processing")
--- buildold/image_processing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brandon/catkin_ws/src/urc-code/src/image_processing
-# Build directory: /home/brandon/catkin_ws/src/urc-code/build/image_processing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
