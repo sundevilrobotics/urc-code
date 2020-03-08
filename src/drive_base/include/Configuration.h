dfb553f98957320f688076841eb9bdf0e89a96f8
Brandon Rice <bjrice13@gmail.com>
2020-03-07 17:13:55 -0700

    Removed old /src/ folder to follow new project format

--- src/drive_base/include/Configuration.h
+++
@@ -1,4 +0,0 @@
-#define METERS_PER_DEGREE 0.001  //Conversion from motor units to native units
-#define TRACK_WIDTH 0.4  //Track width in meters
-#define MAX_SPEED 1  //Max speed in meters per second
-#define MAX_TURN 1  //Max turn in Radians per second
